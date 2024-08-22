82c3e258814fa5764163168ba84e924a86781b77
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-08-22 11:05:49 +0200

    realization app RandomQuoteGenerator

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import RandomQuoteGenerator from './components/RandomQuoteGenerator.jsx'
+
+function App() {
+
+
+  return (
+    <>
+
+    <RandomQuoteGenerator />
+     
+    </>
+  )
+}
+
+export default App
